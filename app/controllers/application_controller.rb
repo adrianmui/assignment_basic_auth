eb75ed3ac6454ed70aac36a7e45e14b651329ac2
Adrian Mui <adrian.mui@gmail.com>
2016-08-10 12:25:35 -0500

    auth is a bitch basic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # http_basic_authenticate_with :name => 'foo',
+  #                              :password => 'bar',
+  #                              :only => [:update, :create]
+  
 
-  http_basic_authenticate_with       :name => "Bob",
-                                     :password => "password",
-                                     :only => [:update, :create]
-  before_action :authenticate_user, except: [:show, :index]
-
+  before_action :authenticate_user, only: [:create, :update, :destroy]
+ 
+  #skip_filter :authenticate_user, :only => :show
 
   private
 
   def authenticate_user
-    authenticate_or_request_with_http_basic('message to user') do |username, password|
-      username == "admin" && password == "password"
+    authenticate_or_request_with_http_basic('Message to User') do |username, password|
+      username == 'admin' && password == 'password'
     end
   end
 end
